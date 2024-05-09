23d5e1f73690ce20723f3a433d082a3fac88b6ef
Alphalikegithub <2908926129@qq.com>
2024-05-09 21:49:12 +0800

    打包了linux下的编译链接后的c++可执行文件，成功在centos上运行，但是Ubuntu上报错

--- myProtocol9/build/export.py
+++ myProtocol9/build/export.py
@@ -4,7 +4,7 @@ import os
 outpath = "../export_lib"   # -- 设置依赖库lib输出路径
 os.makedirs(outpath) if not os.path.exists(outpath) else None
 
-command = "ldd ./server"   # -- xxx更换为要打包的可执行文件名
+command = "ldd ./client"   # -- xxx更换为要打包的可执行文件名
 out_string = os.popen(command)
 data = out_string.readlines()
 data = [[data_.split()[0], data_.split()[2]] for data_ in data if len(data_.split()) == 4]
---
+++ myProtocol9/output/runserver.sh
@@ -0,0 +1,5 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./export_lib/
+./server 127.0.0.1 8089\   # -- 可执行文件
+# ./param1/param1.suffix \   # -- 参数一
+# ./param2/param2.suffix \   # -- 参数二
+# ...
